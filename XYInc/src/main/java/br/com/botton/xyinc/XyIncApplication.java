2f26df202f98e70c2a4c1e3f2a20e4791d36edca
luizbotton <luizbotton@192.168.0.109>
2020-08-04 01:57:55 -0300

    Concluido o frontend, backend, initdb, banco e inclusao de testes.

--- XYInc/src/main/java/br/com/botton/xyinc/XyIncApplication.java
+++ XYInc/src/main/java/br/com/botton/xyinc/XyIncApplication.java
@@ -2,8 +2,14 @@ package br.com.botton.xyinc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan("br.com.botton.xyinc.entity")
+@ComponentScan({"br.com.botton.xyinc.controller","br.com.botton.xyinc.service"})
+@EnableJpaRepositories("br.com.botton.xyinc.repository")
 public class XyIncApplication {
 
 	public static void main(String[] args) {
